90b56d1e36d83a06e8b28f612883776a56418074
Soleiman Ibrahim <soleiman.ibrahim@gmail.com>
2020-09-10 18:29:10 +0200

    initial commit

---
+++ src/PrintMean.java
@@ -0,0 +1,13 @@
+package com.company;
+
+    public class PrintMean {
+
+        public static void main(String[] args) {
+            System.out.println("Mean of 2, 1, 3, 3: " + ((2 + 1 + 3 + 3) / 4));
+            System.out.println("Mean of 1, 2, 3, 4: " + ((1 + 2 + 3 + 4) / 4));
+            System.out.println("Mean of 1, -2, 3, -4: " + ((1 - 2 + 3 - 4) / 4));
+            System.out.println("Mean of -1, -11, 24, 2: " + ((-1 - 11 + 24 + 2) / 4));
+//senaste ändring PrintMEANNEW - Coolt !
+
+    }
+}
