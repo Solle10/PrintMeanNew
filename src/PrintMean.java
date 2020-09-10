e6fedd9563bf3365905728c23690252b34e0dcf2
Soleiman Ibrahim <soleiman.ibrahim@gmail.com>
2020-09-10 18:36:08 +0200

    added 4,4,4,4

--- src/PrintMean.java
+++ src/PrintMean.java
@@ -3,6 +3,7 @@ package com.company;
     public class PrintMean {
 
         public static void main(String[] args) {
+            System.out.println("Mean of 4, 4, 4, 4: " + ((4 + 4 + 4 + 4) / 4));
             System.out.println("Mean of 5, 5, 5, 5: " + ((5 + 5 + 5 + 5) / 4));
             System.out.println("Mean of 2, 1, 3, 3: " + ((2 + 1 + 3 + 3) / 4));
             System.out.println("Mean of 1, 2, 3, 4: " + ((1 + 2 + 3 + 4) / 4));
